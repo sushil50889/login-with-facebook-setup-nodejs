2335a305dd481004b74cb010e4400d9b5c936964
sushil50889 <mandi.sushil@gmail.com>
2018-06-20 22:38:20 +0530

    final7

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@
 
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : 'process.env.APP_ID',
+      appId      : '128580174511565',
       cookie     : true,
       xfbml      : true,
       version    : 'v2.10'
